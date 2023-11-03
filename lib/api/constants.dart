403636cbaf05fa7ac7668edb4b9e470944217065
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-11-03 20:03:05 +0100

    added TMDB API integration

---
+++ lib/api/constants.dart
@@ -0,0 +1,17 @@
+//class used to keep api key and constant useful links to retrieve data from TMDB
+class Constants {
+  static String apiKey = '7deda61e05cd28b32ad0a2b510923eff';
+  static String readAccesToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGVkYTYxZTA1Y2QyOGIzMmFkMGEyYjUxMDkyM2VmZiIsInN1YiI6IjY1NDNlMjJmNDFhNTYxMzM2ODgyNTIzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.caPWqrJ-t62L0AbynTzog1iAusLFsn8Gvsyd_sD2RBE';
+  static String imagePath = 'https://image.tmdb.org/t/p/w500';
+  static String videoPath = '';
+  static String trendingMovie =
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=$apiKey';
+  static String mostPopular =
+      'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey';
+  static String comingSoon = '';
+  static String topRated = '';
+  static String movieCredits = '';
+  static String similarMovies = '';
+  static String movieReviews = '';
+}
