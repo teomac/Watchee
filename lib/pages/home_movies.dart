935142779971ebb99925e5661c5b5b952d5a7cc5
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-11-03 20:03:38 +0100

    created slider widget for trending movies

--- lib/pages/home_movies.dart
+++ lib/pages/home_movies.dart
@@ -1,12 +1,26 @@
 import 'package:dima_project/pages/user_info.dart';
+import 'package:dima_project/widgets/trending_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:dima_project/models/movie.dart';
+import 'package:dima_project/api/tmdb_api.dart';
 
-class HomeMovies extends StatelessWidget {
-  const HomeMovies({Key? key}) : super(key: key);
+class HomeMovies extends StatefulWidget {
+  @override
+  State<HomeMovies> createState() => HomeMoviesState();
+}
+
+class HomeMoviesState extends State<HomeMovies> {
+  late Future<List<Movie>> trendingMovies;
+
+  @override
+  void initState() {
+    super.initState();
+    trendingMovies = fetchTrendingMovies();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      body: Container(
+        body: Container(
           padding: const EdgeInsets.all(40.0),
           child: Column(
             children: [
@@ -32,6 +46,27 @@ class HomeMovies extends StatelessWidget {
                 },
               ),
               UserInfo(),
+              //sized box for the trendingMovies widget.
+              //It needs to be FutureBuilder, because it first need to retrieve data via api
+              SizedBox(
+                child: FutureBuilder(
+                    future: trendingMovies,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text(snapshot.error.toString()),
+                        );
+                      } else if (snapshot.hasData) {
+                        return TrendingSlider(
+                          snapshot: snapshot,
+                        );
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    }),
+              )
             ],
-          )));
+          ),
+        ),
+      );
 }
