a80e2c8c314982f0dbfc540668de633471ac7af5
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2023-11-03 20:03:38 +0100

    created slider widget for trending movies

---
+++ lib/widgets/trending_slider.dart
@@ -0,0 +1,43 @@
+// ignore: depend_on_referenced_packages
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dima_project/api/constants.dart';
+import 'package:flutter/material.dart';
+
+//widget used to display the trending movies with a self moving horizontal slider
+class TrendingSlider extends StatelessWidget {
+  const TrendingSlider({
+    super.key,
+    required this.snapshot,
+  });
+
+  final AsyncSnapshot snapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: CarouselSlider.builder(
+          itemCount: 10,
+          options: CarouselOptions(
+              height: 300,
+              autoPlay: true,
+              viewportFraction: 0.55,
+              enlargeCenterPage: true,
+              pageSnapping: true,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              autoPlayAnimationDuration: const Duration(seconds: 1)),
+          itemBuilder: (context, itemIndex, pageViewIndex) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: SizedBox(
+                  height: 300,
+                  width: 200,
+                  child: Image.network(
+                      filterQuality: FilterQuality.high,
+                      fit: BoxFit.cover,
+                      '${Constants.imagePath}${snapshot.data[itemIndex].posterPath}')),
+            );
+          },
+        ));
+  }
+}
