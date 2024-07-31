d8bff0baf2ff1c833038265c5659cdba7593bc13
Matteo Laini <100694976+matteolaini@users.noreply.github.com>
2024-07-31 16:47:18 +0200

    Added dark/light theme, implemented safe area and other fix

---
+++ lib/pages/reset_password_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dima_project/widgets/my_textfield.dart';
+import 'package:dima_project/widgets/custom_submit_button.dart';
+
+class ResetPasswordPage extends StatefulWidget {
+  const ResetPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  _ResetPasswordPageState createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  final TextEditingController _controllerEmail = TextEditingController();
+  String? errorMessage = '';
+  String? successMessage = '';
+
+  Future<void> resetPassword() async {
+    setState(() {
+      errorMessage = '';
+      successMessage = '';
+    });
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
+    );
+
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: _controllerEmail.text.trim(),
+      );
+      setState(() {
+        successMessage = 'Password reset email sent. Check your inbox.';
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    } catch (e) {
+      setState(() {
+        errorMessage = 'An error occurred. Please try again.';
+      });
+    }
+
+    Navigator.of(context).pop(); // Dismiss the loading dialog
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Reset Password'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Please enter the email associated with your account.',
+                  style: TextStyle(fontSize: 18),
+                  //make the text red
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                MyTextField(
+                  controller: _controllerEmail,
+                  title: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 20),
+                CustomSubmitButton(
+                  text: 'Reset Password',
+                  onPressed: resetPassword,
+                ),
+                const SizedBox(height: 20),
+                if (errorMessage != '')
+                  Text(
+                    errorMessage!,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                if (successMessage != '')
+                  Text(
+                    successMessage!,
+                    style: const TextStyle(color: Colors.green),
+                  ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
