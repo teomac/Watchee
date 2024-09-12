33686a054f9ece54dcc2824b4178336377086aac
magicteo <matty.macaluso@gmail.com>
2024-09-12 17:56:55 +0200

    added review system

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io'; // Add this line to import the 'File' class
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dima_project/models/movie_review.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:dima_project/models/user_model.dart';
@@ -218,6 +219,44 @@ class UserService {
       logger.d('Failed to upload image: $e');
       return null;
     }
+  }
+
+  // add a new movie review
+  Future<void> addMovieReview(
+      String userId, int movieId, int rating, String text) async {
+    final review = MovieReview(
+      id: _firestore.collection('reviews').doc().id,
+      userId: userId,
+      movieId: movieId,
+      text: text,
+      rating: rating,
+      timestamp: Timestamp.now(),
+    );
+
+    await _firestore.collection('reviews').doc(review.id).set(review.toMap());
+  }
+
+  // retrieve the two most recent reviews for a movie
+  Future<List<MovieReview>> getReviewsForMovie(String movieId) async {
+    QuerySnapshot snapshot = await _firestore
+        .collection('reviews')
+        .where('movieId', isEqualTo: movieId)
+        .orderBy('timestamp', descending: true)
+        .limit(2)
+        .get();
+
+    return snapshot.docs.map((doc) => MovieReview.fromFirestore(doc)).toList();
+  }
+
+  // retrieve reviews for a user
+  Future<List<MovieReview>> getReviewsByUser(String userId) async {
+    QuerySnapshot snapshot = await _firestore
+        .collection('reviews')
+        .where('movieId', isEqualTo: userId)
+        .orderBy('timestamp', descending: true)
+        .get();
+
+    return snapshot.docs.map((doc) => MovieReview.fromFirestore(doc)).toList();
   }
 
   Future<bool> signOut() async {
