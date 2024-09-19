bf71d5c15b84052f719fe0786c4f33f06b78e0ab
magicteo <matty.macaluso@gmail.com>
2024-09-19 15:35:18 +0200

    solved warning

--- lib/pages/account/notifications_page.dart
+++ lib/pages/account/notifications_page.dart
@@ -2,7 +2,6 @@ import 'package:dima_project/models/user_model.dart';
 import 'package:dima_project/services/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 
 class NotificationsPage extends StatefulWidget {
